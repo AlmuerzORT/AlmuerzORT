963b8a77d7f71bd47e01256e47a9103d93fff6af
JOAQUIN IGNACIO ORDO�EZ <48240825@ort>
2024-11-14 11:30:01 -0300

    css parcial

--- Models/Restricciones.cs
+++ Models/Restricciones.cs
@@ -1,8 +1,8 @@
-public class Restricciones(){
+public class Restricciones{
     public int id_restriccion {get; set;}
     public string nombre{get; set;}
 
-  public Restricciones(){} //VER PORQUE NO ANDA CONTRUCTOR
+  public Restricciones(){}
 
     public Restricciones(string nom){
         nombre =  nom;
